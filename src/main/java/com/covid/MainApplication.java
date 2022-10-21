226c92d4e0db24d558f5071df84dc4744095684e
bertrand <673809859@qq.com>
2022-10-21 22:32:19 +0800

    first commit

---
+++ src/main/java/com/covid/MainApplication.java
@@ -0,0 +1,16 @@
+package com.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Michael Hunger
+ * @author Mark Angrish
+ * @author Michael J. Simons
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
